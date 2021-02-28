005525d65879a9c39c094cfac73b67502dc7b2ec
Manderson202 <manderson202@hotmail.com>
2021-02-28 18:52:52 +1100

    trial and error

--- scripts/example.js
+++ scripts/example.js
@@ -60,7 +60,7 @@ robot.respond(/Roll dice/i, (res) => {
   var dice2 = Math.floor(Math.random() * 6) + 1;
   var totalScore = dice1 + dice2;
   res.send(`You have rolled ${dice1} and ${dice2}, your total score is ${totalScore}`);
-  if (totalScore < 10) {
+  if (totalScore > 10) {
     res.send(`You win with a score of ${totalScore}`);
   } else {
     res.send("Try again");
