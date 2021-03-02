e2f97240f833bfb9c16a44efb01dd3fdf54a19a0
Manderson202 <manderson202@hotmail.com>
2021-03-02 15:37:32 +1100

    trial and error; git commit -m trial and error

--- scripts/example.js
+++ scripts/example.js
@@ -68,8 +68,7 @@ robot.respond(/Roll dice/i, (res) => {
   var dice2 = Math.floor(Math.random() * 6) + 1;
   var totalScore = dice1 + dice2;
   var scoreArr = [];
-  res.send(`You have rolled ${dice1} and ${dice2}, your total score is ${totalScore}`);
-  scoreArr.push(totalScore);
+  res.send(`You have rolled ${dice1} and ${dice2}, your total score is ${scoreArr.push(totalScore)}`);
   if (scoreArr.length = 5){
     res.send(`You win with a score of ${Math.max(scoreArr)}`)
   }
